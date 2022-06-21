053cf28b802b2be79d4dd28894209dc7bfac132f
vasukisravanth <vasukisravanth26@gmail.com>
2022-06-22 00:25:24 +0530

    finalchanges

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -6,19 +6,24 @@ let flag=tokenroutecontroller.flag
 
 
 //const isAuth=tokenroutecontroller.isAuth;
-const user_info=(req,res,isauth)=>{
-    var unum=req.params.unum;
-    
-        userinfo.find({userin:unum},function(err,userdata){
-            res.render('user',{user:unum,previousBookings:userdata});
-        });
-    
-    
-       // res.render('login');
-    
-   
-    
-    
+const user_info = (req, res, isauth) => {
+    var unum = req.params.unum;
+
+    userinfo.find({ userin: unum }, function (err, userdata) {
+        
+            res.render('user', { user: unum, previousBookings: userdata, phonenumberuser: unum });
+
+        
+
+
+    });
+
+
+    // res.render('login');
+
+
+
+
 };
 
 
